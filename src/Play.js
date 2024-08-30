f9528494a318993d4a017dc9270c9e6a500f7306
“Miguel <erqmac@gmail.com>
2024-08-30 16:47:32 -0700

    rngolf done

--- src/Play.js
+++ src/Play.js
@@ -1,102 +1,120 @@
 class Play extends Phaser.Scene {
-    constructor() {
-        super('playScene')
-        this.shotsTaken=0;
-        this.score=0;
-    }
+  constructor() {
+    super("playScene");
+    this.shotsTaken = 0;
+    this.score = 0;
+  }
 
-    init() {
-        // useful variables
-        this.SHOT_VELOCITY_X=200
-        this.SHOT_VELOCITY_Y_MIN=700
-        this.SHOT_VELOCITY_Y_MAX=1100
+  init() {
+    // useful variables
+    this.SHOT_VELOCITY_X = 200;
+    this.SHOT_VELOCITY_Y_MIN = 700;
+    this.SHOT_VELOCITY_Y_MAX = 1100;
+  }
 
-    }
+  preload() {
+    this.load.path = "./assets/img/";
+    this.load.image("grass", "grass.jpg");
+    this.load.image("cup", "cup.jpg");
+    this.load.image("ball", "ball.png");
+    this.load.image("wall", "wall.png");
+    this.load.image("oneway", "one_way_wall.png");
+  }
 
-    preload() {
-        this.load.path = './assets/img/'
-        this.load.image('grass', 'grass.jpg')
-        this.load.image('cup', 'cup.jpg')
-        this.load.image('ball', 'ball.png')
-        this.load.image('wall', 'wall.png')
-        this.load.image('oneway', 'one_way_wall.png')
-    }
+  create() {
+    // add background grass
+    this.grass = this.add.image(0, 0, "grass").setOrigin(0);
 
-    create() {
-        // add background grass
-        this.grass = this.add.image(0, 0, 'grass').setOrigin(0)
+    // add cup
+    this.cup = this.physics.add.sprite(width / 2, height / 10, "cup");
+    this.cup.body.setCircle(this.cup.width / 4);
+    this.cup.body.setOffset(this.cup.width / 4);
+    this.cup.body.setImmovable(true);
+    // add ball
+    this.ball = this.physics.add.sprite(
+      width / 2,
+      height - height / 10,
+      "ball"
+    );
+    this.ball.body.setCircle(this.ball.width / 2);
+    this.ball.body.setCollideWorldBounds(true);
+    this.ball.body.setBounce(0.5);
+    this.ball.body.setDamping(true).setDrag(0.5);
+    // add walls
+    //fixed scattering the walls
+    //used Prettier plugin to format the code
+    let wallA = this.physics.add.sprite(0, height / 6, "wall");
+    wallA.setX(Phaser.Math.Between(width / 4, (3 * width) / 4));
+    wallA.body.setImmovable(true);
 
-        // add cup
-        this.cup = this.physics.add.sprite(width/2,height/10,'cup')
-        this.cup.body.setCircle(this.cup.width/4)
-        this.cup.body.setOffset(this.cup.width/4)
-        this.cup.body.setImmovable(true)
-        // add ball
-        this.ball=this.physics.add.sprite(width/2,height-height/10,'ball')
-        this.ball.body.setCircle(this.ball.width/2)
-        this.ball.body.setCollideWorldBounds(true)
-        this.ball.body.setBounce(0.5)
-        this.ball.body.setDamping(true).setDrag(0.5)
-        // add walls
-        let wallA=this.physics.add.sprite(0,height/4,'wall')
-        wallA.setX(Phaser.Math.Between(0+wallA.width/2,width=wallA.width/2))
-        wallA.body.setImmovable(true)
+    let wallB = this.physics.add.sprite(0, height / 2, "wall");
+    wallB.setX(Phaser.Math.Between(width / 4, (3 * width) / 4));
+    wallB.body.setImmovable(true);
 
-        let wallB=this.physics.add.sprite(0,height/2,'wall')
-        wallB.setX(Phaser.Math.Between(0+wallA.width/2,width=wallA.width/2))
-        wallB.body.setImmovable(true)
+    this.walls = this.add.group([wallA, wallB]);
+    // add one-way
+    this.oneWay = this.physics.add.sprite(width / 2, height / 4 + 3, "oneway");
+    this.oneWay.setX(
+      Phaser.Math.Between(
+        0 + this.oneWay.width / 2,
+        width - this.oneWay.width / 2
+      )
+    );
+    this.oneWay.body.setImmovable(true);
+    this.oneWay.body.checkCollision.down = false;
+    // add pointer input
+    this.input.on("pointerdown", (pointer) => {
+      this.shotsTaken++;
+      let shotDirection = pointer.y <= this.ball.y ? 1 : -1;
+      let shotXDirection = pointer.x - this.ball.x;
+      this.ball.body.setVelocityX(shotXDirection * 3);
+      this.ball.body.setVelocityY(
+        Phaser.Math.Between(
+          this.SHOT_VELOCITY_Y_MIN,
+          this.SHOT_VELOCITY_Y_MAX
+        ) * shotDirection
+      );
+    });
+    // cup/ball collision
+    this.physics.add.collider(this.ball, this.cup, (ball, cup) => {
+      this.score++; // gain points
+      ball.setPosition(width / 2, height - height / 10); // reset
+      ball.body.setVelocity(0, 0);
+    });
 
-        this.walls=this.add.group([wallA,wallB])
-        // add one-way
-        this.oneWay=this.physics.add.sprite(width/2,height/4+3,'oneway')
-        this.oneWay.setX(Phaser.Math.Between(0+this.oneWay.width/2,width-this.oneWay.width/2))
-        this.oneWay.body.setImmovable(true)
-        this.oneWay.body.checkCollision.down=false
-        // add pointer input
-        this.input.on('pointerdown', (pointer) => {
-            this.shotsTaken++; 
-            let shotDirection = pointer.y <= this.ball.y ? 1 : -1;
-            let shotXDirection = pointer.x - this.ball.x;
-            this.ball.body.setVelocityX(shotXDirection * 3); 
-            this.ball.body.setVelocityY(Phaser.Math.Between(this.SHOT_VELOCITY_Y_MIN, this.SHOT_VELOCITY_Y_MAX) * shotDirection);
-        });
-        // cup/ball collision
-        this.physics.add.collider(this.ball, this.cup, (ball, cup) => {
-            this.score++; // gain points
-            ball.setPosition(width / 2, height - height / 10); // reset
-            ball.body.setVelocity(0, 0); 
-        })
+    //moving walls
+    this.tweens.add({
+      targets: wallA,
+      wallB,
+      x: { from: 0, to: width },
+      yoyo: true,
+      repeat: -1,
+      ease: "Sine.easeInOut",
+      duration: 4000,
+    });
 
-        //moving walls
-        this.tweens.add({
-            targets: wallA,wallB,
-            x: {from: 0, to: width},
-            yoyo: true,
-            repeat: -1, 
-            ease: 'Sine.easeInOut',
-            duration: 4000, 
-        });
+    // ball/wall collision
+    this.physics.add.collider(this.ball, this.walls);
+    // ball/one-way collision
+    this.physics.add.collider(this.ball, this.oneWay);
+    this.shotText = this.add.text(10, 10, "", {
+      fontSize: "16px",
+      fill: "#FFF",
+    });
+  }
 
-        // ball/wall collision
-        this.physics.add.collider(this.ball,this.walls)
-        // ball/one-way collision
-        this.physics.add.collider(this.ball,this.oneWay)
-        this.shotText = this.add.text(10, 10, '', { fontSize: '16px', fill: '#FFF' });
-
-    }
-
-    update() {
-        this.shotText.setText(`Shots: ${this.shotsTaken} Score: ${this.score} Accuracy: ${this.shotsTaken > 0 ? Math.round((this.score / this.shotsTaken) * 100) : 0}%`);
-
-    }
-
-    
+  update() {
+    this.shotText.setText(
+      `Shots: ${this.shotsTaken} Score: ${this.score} Accuracy: ${
+        this.shotsTaken > 0
+          ? Math.round((this.score / this.shotsTaken) * 100)
+          : 0
+      }%`
+    );
+  }
 }
 
-
 // Correctly defining the ballReset method inside the Play class
-
-
 
 /*
 CODE CHALLENGE
